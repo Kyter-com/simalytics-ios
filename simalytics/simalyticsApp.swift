6a1610a012802fd62f2a7641c57ce9f92e12dafe
Nick Reisenauer <nickdreisenauer@gmail.com>
2025-02-24 14:55:09 -0700

    Initial commit

---
+++ simalytics/simalyticsApp.swift
@@ -0,0 +1,17 @@
+//
+//  simalyticsApp.swift
+//  simalytics
+//
+//  Created by Nick Reisenauer on 2/24/25.
+//
+
+import SwiftUI
+
+@main
+struct simalyticsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ simalyticsTests/simalyticsTests.swift
@@ -0,0 +1,16 @@
+//
+//  simalyticsTests.swift
+//  simalyticsTests
+//
+//  Created by Nick Reisenauer on 2/24/25.
+//
+
+import Testing
+
+struct simalyticsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
