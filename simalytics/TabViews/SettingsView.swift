a4d6f6f8f9d275650e24531650fcb07931ca2605
Nick Reisenauer <nickdreisenauer@gmail.com>
2025-02-25 11:21:06 -0700

    fix: naming and sign in

--- simalytics/TabViews/SettingsView.swift
+++ simalytics/TabViews/SettingsView.swift
@@ -5,17 +5,32 @@
 //  Created by Nick Reisenauer on 2/24/25.
 //
 
+import AuthenticationServices
 import SwiftUI
 
 struct SettingsView: View {
+  @Environment(\.webAuthenticationSession) private var webAuthenticationSession
+
   var body: some View {
     NavigationView {
       VStack {
         Text("Settings View!")
-        Button(action: {
-
-        }) {
-          Text("Login to Simkl")
+        Button("Sign In") {
+          Task {
+            do {
+              let oauthURL = URL(
+                string:
+                  "https://simkl.com/oauth/authorize?client_id=c387a1e6b5cf2151af039a466c49a6b77891a4134aed1bcb1630dd6b8f0939c9&response_type=code&redirect_uri=simalytics://"
+              )!
+              let urlWithToken = try await webAuthenticationSession.authenticate(
+                using: oauthURL,
+                callbackURLScheme: "simalytics"
+              )
+              print("URL with token: \(urlWithToken)")
+            } catch {
+              print("Error: \(error)")
+            }
+          }
         }
       }
       .navigationTitle("Settings")
