b8dd708f34dc29f1ca88b6060ec06d545eb872d7
Nick Reisenauer <nickdreisenauer@gmail.com>
2025-03-03 13:56:28 -0700

    temporary binding

--- simalytics/MainView.swift
+++ simalytics/MainView.swift
@@ -4,14 +4,30 @@
 //
 //  Created by Nick Reisenauer on 2/24/25.
 //
-
 import SwiftUI
+
+/// I have a Binding because I want to detect if the same tab is tapped multiple times in a row.
+extension Binding {
+  func onUpdate() -> Binding<Value> {
+    Binding(
+      get: {
+        self.wrappedValue
+      },
+      set: { newValue in
+        self.wrappedValue = newValue
+        print("tab clicked", newValue)
+      }
+    )
+  }
+}
 
 struct MainView: View {
   @State private var selectedTab = 0
 
   var body: some View {
-    TabView(selection: $selectedTab) {
+    TabView(
+      selection: $selectedTab.onUpdate()
+    ) {
       HomeView()
         .tabItem {
           Label("Home", systemImage: "play.house.fill")
