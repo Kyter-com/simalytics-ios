75e15f9373bf195d9a109ad08191eed48a6342c7
Nick Reisenauer <nickdreisenauer@gmail.com>
2025-03-11 08:51:27 -0700

    fix: remove binding

--- simalytics/MainView.swift
+++ simalytics/MainView.swift
@@ -7,33 +7,11 @@
 
 import SwiftUI
 
-extension Binding {
-  func onUpdate(_ closure: @escaping (Value) -> Void) -> Binding<Value> {
-    Binding(
-      get: {
-        self.wrappedValue
-      },
-      set: { newValue in
-        self.wrappedValue = newValue
-        closure(newValue)
-      }
-    )
-  }
-}
-
 struct MainView: View {
   @State private var selectedTab = 0
-  @State private var lastSelectedTab = 0
 
   var body: some View {
-    TabView(
-      selection: $selectedTab.onUpdate { newValue in
-        if newValue == lastSelectedTab && newValue == 1 {
-          print("search tapped twice")
-        }
-        lastSelectedTab = newValue
-      }
-    ) {
+    TabView(selection: $selectedTab) {
       HomeView()
         .tabItem {
           Label("Home", systemImage: "play.house.fill")
