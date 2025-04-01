c725b660c2a8727ff79e31d2adcc20f48f9b6e14
Nick Reisenauer <nickdreisenauer@gmail.com>
2025-04-01 09:20:13 -0700

    feat: episode model

---
+++ simalytics/Models/Anime/AnimeEpisodeModel.swift
@@ -0,0 +1,23 @@
+//
+//  AnimeEpisodeModel.swift
+//  simalytics
+//
+//  Created by Nick Reisenauer on 4/1/25.
+//
+
+import Foundation
+
+struct AnimeEpisodeModel: Codable {
+  let title: String
+  let description: String?
+  let episode: Int?
+  let type: String?
+  let aired: Bool?
+  let img: String?
+  let date: String?
+  let ids: AnimeEpisodeModelIds
+}
+
+struct AnimeEpisodeModelIds: Codable {
+  let simkl_id: Int
+}
