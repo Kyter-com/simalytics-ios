26eb049e87dd55b12bcf1b2b719c3a5635fff797
Nick Reisenauer <nickdreisenauer@gmail.com>
2025-04-24 13:30:43 -0700

    fix: update rated at

--- simalytics/ViewModels/Sync.swift
+++ simalytics/ViewModels/Sync.swift
@@ -326,6 +326,7 @@ func fetchAndStoreMoviesRemovedFromList(_ accessToken: String, _ lastActivity: S
 
 func fetchAndStoreMoviesRatedAt(_ accessToken: String, _ lastActivity: String?, _ context: ModelContext) async {
   guard let lastActivity = lastActivity else { return }
+  let formatter = ISO8601DateFormatter()
 
   do {
     var syncRecord = try context.fetch(
@@ -335,11 +336,61 @@ func fetchAndStoreMoviesRatedAt(_ accessToken: String, _ lastActivity: String?, 
       syncRecord = V1.SDLastSync(id: 1)
       context.insert(syncRecord!)
     }
+
     if lastActivity == syncRecord!.movies_rated_at { return }
 
-    await fetchAndStoreMoviesPlanToWatch(accessToken, lastActivity, context)
-    await fetchAndStoreMoviesDropped(accessToken, lastActivity, context)
-    await fetchAndStoreMoviesCompleted(accessToken, lastActivity, context)
+    var endpoint = URLComponents(string: "https://api.simkl.com/sync/ratings/movies?memos=yes")!
+
+    let lastActivityDate = formatter.date(from: lastActivity)!
+    if let previousSyncDate = syncRecord!.movies_rated_at.flatMap(formatter.date(from:)) {
+      if lastActivityDate > previousSyncDate {
+        let dateFrom = formatter.string(
+          from: Calendar.current.date(byAdding: .minute, value: -5, to: previousSyncDate)!)
+        endpoint = URLComponents(
+          string:
+            "https://api.simkl.com/sync/ratings/movies?memos=yes&date_from=\(dateFrom)")!
+      }
+    }
+
+    print("\(endpoint.url!)")
+    var request = URLRequest(url: endpoint.url!)
+    request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+    request.setValue(SIMKL_CLIENT_ID, forHTTPHeaderField: "simkl-api-key")
+    request.setValue("Bearer \(accessToken)", forHTTPHeaderField: "Authorization")
+
+    let (data, _) = try await URLSession.shared.data(for: request)
+    guard let result = try? JSONDecoder().decode(MoviesModel.self, from: data) else {
+      syncRecord!.movies_rated_at = lastActivity
+      try context.save()
+      return
+    }
+
+    let movies = result.movies ?? []
+    for movieItem in movies {
+      context.insert(
+        V1.SDMovies(
+          simkl: (movieItem.movie?.ids?.simkl)!,
+          title: movieItem.movie?.title,
+          added_to_watchlist_at: movieItem.added_to_watchlist_at,
+          last_watched_at: movieItem.last_watched_at,
+          user_rated_at: movieItem.user_rated_at,
+          status: movieItem.status,
+          user_rating: movieItem.user_rating,
+          poster: movieItem.movie?.poster,
+          year: movieItem.movie?.year,
+          id_slug: movieItem.movie?.ids?.slug,
+          id_tvdbmslug: movieItem.movie?.ids?.tvdbmslug,
+          id_imdb: movieItem.movie?.ids?.imdb,
+          id_offen: movieItem.movie?.ids?.offen,
+          id_traktslug: movieItem.movie?.ids?.traktslug,
+          id_letterslug: movieItem.movie?.ids?.letterslug,
+          id_jwslug: movieItem.movie?.ids?.jwslug,
+          id_tmdb: movieItem.movie?.ids?.tmdb,
+          memo_text: movieItem.movie?.memo?.text,
+          memo_is_private: movieItem.movie?.memo?.is_private
+        )
+      )
+    }
 
     syncRecord!.movies_rated_at = lastActivity
     try context.save()
@@ -811,22 +862,77 @@ func fetchAndStoreTVRemovedFromList(_ accessToken: String, _ lastActivity: Strin
 
 func fetchAndStoreTVRatedAt(_ accessToken: String, _ lastActivity: String?, _ context: ModelContext) async {
   guard let lastActivity = lastActivity else { return }
+  let formatter = ISO8601DateFormatter()
 
   do {
-    var syncRecord = try context.fetch(
-      FetchDescriptor<V1.SDLastSync>(predicate: #Predicate { $0.id == 1 })
-    ).first
+    var syncRecord = try context.fetch(FetchDescriptor<V1.SDLastSync>(predicate: #Predicate { $0.id == 1 })).first
     if syncRecord == nil {
       syncRecord = V1.SDLastSync(id: 1)
       context.insert(syncRecord!)
     }
+
     if lastActivity == syncRecord!.tv_rated_at { return }
 
-    await fetchAndStoreTVPlanToWatch(accessToken, lastActivity, context)
-    await fetchAndStoreTVDropped(accessToken, lastActivity, context)
-    await fetchAndStoreTVCompleted(accessToken, lastActivity, context)
-    await fetchAndStoreTVWatching(accessToken, lastActivity, context)
-    await fetchAndStoreTVHold(accessToken, lastActivity, context)
+    var endpoint = URLComponents(string: "https://api.simkl.com/sync/ratings/tv?memos=yes")!
+
+    let lastActivityDate = formatter.date(from: lastActivity)!
+    if let previousSyncDate = syncRecord!.tv_rated_at.flatMap(formatter.date(from:)) {
+      if lastActivityDate > previousSyncDate {
+        let dateFrom = formatter.string(
+          from: Calendar.current.date(byAdding: .minute, value: -5, to: previousSyncDate)!)
+        endpoint = URLComponents(
+          string:
+            "https://api.simkl.com/sync/ratings/tv?memos=yes&date_from=\(dateFrom)"
+        )!
+      }
+    }
+
+    print("\(endpoint.url!)")
+    var request = URLRequest(url: endpoint.url!)
+    request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+    request.setValue(SIMKL_CLIENT_ID, forHTTPHeaderField: "simkl-api-key")
+    request.setValue("Bearer \(accessToken)", forHTTPHeaderField: "Authorization")
+
+    let (data, _) = try await URLSession.shared.data(for: request)
+    guard let result = try? JSONDecoder().decode(TVModel.self, from: data) else {
+      syncRecord!.tv_rated_at = lastActivity
+      try context.save()
+      return
+    }
+
+    let shows = result.shows ?? []
+    for showItem in shows {
+      context.insert(
+        V1.SDShows(
+          simkl: (showItem.show?.ids?.simkl)!,
+          added_to_watchlist_at: showItem.added_to_watchlist_at,
+          last_watched_at: showItem.last_watched_at,
+          user_rated_at: showItem.user_rated_at,
+          user_rating: showItem.user_rating,
+          status: showItem.status,
+          last_watched: showItem.last_watched,
+          next_to_watch: showItem.next_to_watch,
+          watched_episodes_count: showItem.watched_episodes_count,
+          total_episodes_count: showItem.total_episodes_count,
+          not_aired_episodes_count: showItem.not_aired_episodes_count,
+          title: showItem.show?.title,
+          poster: showItem.show?.poster,
+          year: showItem.show?.year,
+          memo_text: showItem.show?.memo?.text,
+          memo_is_private: showItem.show?.memo?.is_private,
+          id_slug: showItem.show?.ids?.slug,
+          id_offen: showItem.show?.ids?.offen,
+          id_tvdbslug: showItem.show?.ids?.tvdbslug,
+          id_instagram: showItem.show?.ids?.instagram,
+          id_tw: showItem.show?.ids?.tw,
+          id_imdb: showItem.show?.ids?.imdb,
+          id_tmdb: showItem.show?.ids?.tmdb,
+          id_traktslug: showItem.show?.ids?.traktslug,
+          id_jwslug: showItem.show?.ids?.jwslug,
+          id_tvdb: showItem.show?.ids?.tvdb
+        )
+      )
+    }
 
     syncRecord!.tv_rated_at = lastActivity
     try context.save()
@@ -1338,22 +1444,85 @@ func fetchAndStoreAnimeRemovedFromList(_ accessToken: String, _ lastActivity: St
 
 func fetchAndStoreAnimeRatedAt(_ accessToken: String, _ lastActivity: String?, _ context: ModelContext) async {
   guard let lastActivity = lastActivity else { return }
+  let formatter = ISO8601DateFormatter()
 
   do {
-    var syncRecord = try context.fetch(
-      FetchDescriptor<V1.SDLastSync>(predicate: #Predicate { $0.id == 1 })
-    ).first
+    var syncRecord = try context.fetch(FetchDescriptor<V1.SDLastSync>(predicate: #Predicate { $0.id == 1 })).first
     if syncRecord == nil {
       syncRecord = V1.SDLastSync(id: 1)
       context.insert(syncRecord!)
     }
+
     if lastActivity == syncRecord!.anime_rated_at { return }
 
-    await fetchAndStoreAnimePlanToWatch(accessToken, lastActivity, context)
-    await fetchAndStoreAnimeDropped(accessToken, lastActivity, context)
-    await fetchAndStoreAnimeCompleted(accessToken, lastActivity, context)
-    await fetchAndStoreAnimeWatching(accessToken, lastActivity, context)
-    await fetchAndStoreAnimeHold(accessToken, lastActivity, context)
+    var endpoint = URLComponents(string: "https://api.simkl.com/sync/ratings/anime?memos=yes")!
+
+    let lastActivityDate = formatter.date(from: lastActivity)!
+    if let previousSyncDate = syncRecord!.anime_rated_at.flatMap(formatter.date(from:)) {
+      if lastActivityDate > previousSyncDate {
+        let dateFrom = formatter.string(
+          from: Calendar.current.date(byAdding: .minute, value: -5, to: previousSyncDate)!)
+        endpoint = URLComponents(
+          string:
+            "https://api.simkl.com/sync/ratings/anime?memos=yes&date_from=\(dateFrom)"
+        )!
+      }
+    }
+
+    print("\(endpoint.url!)")
+    var request = URLRequest(url: endpoint.url!)
+    request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+    request.setValue(SIMKL_CLIENT_ID, forHTTPHeaderField: "simkl-api-key")
+    request.setValue("Bearer \(accessToken)", forHTTPHeaderField: "Authorization")
+
+    let (data, _) = try await URLSession.shared.data(for: request)
+    guard let result = try? JSONDecoder().decode(AnimeModel.self, from: data) else {
+      syncRecord!.anime_rated_at = lastActivity
+      try context.save()
+      return
+    }
+
+    let animes = result.anime ?? []
+    for animeItem in animes {
+      context.insert(
+        V1.SDAnimes(
+          simkl: (animeItem.show?.ids.simkl)!,
+          added_to_watchlist_at: animeItem.added_to_watchlist_at,
+          last_watched_at: animeItem.last_watched_at,
+          user_rated_at: animeItem.user_rated_at,
+          user_rating: animeItem.user_rating,
+          status: animeItem.status,
+          last_watched: animeItem.last_watched,
+          next_to_watch: animeItem.next_to_watch,
+          watched_episodes_count: animeItem.watched_episodes_count,
+          total_episodes_count: animeItem.total_episodes_count,
+          not_aired_episodes_count: animeItem.not_aired_episodes_count,
+          anime_type: animeItem.anime_type,
+          poster: animeItem.show?.poster,
+          year: animeItem.show?.year,
+          title: animeItem.show?.title,
+          id_slug: animeItem.show?.ids.slug,
+          id_offjp: animeItem.show?.ids.offjp,
+          id_ann: animeItem.show?.ids.ann,
+          id_mal: animeItem.show?.ids.mal,
+          id_anfo: animeItem.show?.ids.anfo,
+          id_offen: animeItem.show?.ids.offen,
+          id_wikien: animeItem.show?.ids.wikien,
+          id_wikijp: animeItem.show?.ids.wikijp,
+          id_allcin: animeItem.show?.ids.allcin,
+          id_imdb: animeItem.show?.ids.imdb,
+          id_tmdb: animeItem.show?.ids.tmdb,
+          id_animeplanet: animeItem.show?.ids.animeplanet,
+          id_anisearch: animeItem.show?.ids.anisearch,
+          id_kitsu: animeItem.show?.ids.kitsu,
+          id_livechart: animeItem.show?.ids.livechart,
+          id_traktslug: animeItem.show?.ids.traktslug,
+          id_letterslug: animeItem.show?.ids.letterslug,
+          id_jwslug: animeItem.show?.ids.jwslug,
+          id_anidb: animeItem.show?.ids.anidb
+        )
+      )
+    }
 
     syncRecord!.anime_rated_at = lastActivity
     try context.save()
