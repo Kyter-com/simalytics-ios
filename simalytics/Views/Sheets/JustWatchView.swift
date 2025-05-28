4c9c9735860815fc54de222811f399d86974ee7d
Nick Reisenauer <nickdreisenauer@gmail.com>
2025-05-27 18:51:46 -0700

    feat: better where to watch

--- simalytics/Views/Sheets/JustWatchView.swift
+++ simalytics/Views/Sheets/JustWatchView.swift
@@ -17,26 +17,70 @@ struct JustWatchView: View {
         if let free = justWatchListings?.free, !free.isEmpty {
           Text("Watch Free")
             .font(.headline)
-          ForEach(free, id: \.provider_id) { option in
-            Text(option.provider_name ?? "")
+            .frame(maxWidth: .infinity, alignment: .leading)
+            .padding(.leading)
+          ScrollView(.horizontal, showsIndicators: false) {
+            HStack(spacing: 16) {
+              ForEach(free, id: \.provider_id) { option in
+                VStack {
+                  CustomKFImage(
+                    imageUrlString: "https://media.themoviedb.org/t/p/original/\(option.logo_path ?? "")",
+                    memoryCacheOnly: true,
+                    height: 50,
+                    width: 50
+                  )
+                }
+              }
+            }
+            .padding([.leading, .trailing, .bottom])
           }
         }
 
         if let flatrate = justWatchListings?.flatrate, !flatrate.isEmpty {
           Text("Stream")
             .font(.headline)
-          ForEach(flatrate, id: \.provider_id) { option in
-            Text(option.provider_name ?? "")
+            .frame(maxWidth: .infinity, alignment: .leading)
+            .padding(.leading)
+          ScrollView(.horizontal, showsIndicators: false) {
+            HStack(spacing: 16) {
+              ForEach(flatrate, id: \.provider_id) { option in
+                VStack {
+                  CustomKFImage(
+                    imageUrlString: "https://media.themoviedb.org/t/p/original/\(option.logo_path ?? "")",
+                    memoryCacheOnly: true,
+                    height: 50,
+                    width: 50
+                  )
+                }
+              }
+            }
+            .padding([.leading, .trailing, .bottom])
           }
         }
 
         if let buy = justWatchListings?.buy, !buy.isEmpty {
           Text("Buy")
             .font(.headline)
-          ForEach(buy, id: \.provider_id) { option in
-            Text(option.provider_name ?? "")
+            .frame(maxWidth: .infinity, alignment: .leading)
+            .padding(.leading)
+          ScrollView(.horizontal, showsIndicators: false) {
+            HStack(spacing: 16) {
+              ForEach(buy, id: \.provider_id) { option in
+                VStack {
+                  CustomKFImage(
+                    imageUrlString: "https://media.themoviedb.org/t/p/original/\(option.logo_path ?? "")",
+                    memoryCacheOnly: true,
+                    height: 50,
+                    width: 50
+                  )
+                }
+              }
+            }
+            .padding([.leading, .trailing, .bottom])
           }
         }
+
+        Spacer()
       }
       .navigationTitle("Where to Watch")
       .navigationBarTitleDisplayMode(.inline)
