75637a3b204aab5c3bab766365297cbb7f6111bf
Nick Reisenauer <nickdreisenauer@gmail.com>
2025-06-13 15:59:02 -0700

    fix: specials view

--- simalytics/Views/Sheets/AnimeEpisodeView.swift
+++ simalytics/Views/Sheets/AnimeEpisodeView.swift
@@ -34,61 +34,65 @@ struct AnimeEpisodeView: View {
           .frame(maxWidth: .infinity, alignment: .leading)
       }
 
-      HStack {
-        Text("S\(episode?.season ?? 0) E\(episode?.episode ?? 0)")
-          .font(.caption)
-          .bold()
-        Text(episode?.date?.formattedEpisodeDate() ?? "")
-          .font(.caption)
-        Spacer()
-      }
-      .padding([.top, .bottom], 2)
-
-      Button(action: {
-        Task {
-          if !hasWatched {
-            await AnimeDetailView.markEpisodeWatched(
-              auth.simklAccessToken,
-              animeDetails?.title ?? "",
-              simklId,
-              episode?.season ?? 0,
-              episode?.episode ?? 0
-            )
-            if let episode = episode {
-              if let index = animeWatchlist?.seasons?.firstIndex(where: { $0.number == episode.season }) {
-                if let episodeIndex = animeWatchlist?.seasons?[index].episodes?.firstIndex(where: { $0.number == episode.episode }) {
-                  animeWatchlist?.seasons?[index].episodes?[episodeIndex].watched = true
-                }
-              }
-            }
-          } else {
-            await AnimeDetailView.markEpisodeUnwatched(
-              auth.simklAccessToken,
-              animeDetails?.title ?? "",
-              simklId,
-              episode?.season ?? 0,
-              episode?.episode ?? 0
-            )
-            if let episode = episode {
-              if let index = animeWatchlist?.seasons?.firstIndex(where: { $0.number == episode.season }) {
-                if let episodeIndex = animeWatchlist?.seasons?[index].episodes?.firstIndex(where: { $0.number == episode.episode }) {
-                  animeWatchlist?.seasons?[index].episodes?[episodeIndex].watched = false
-                }
-              }
-            }
-          }
-          await syncLatestActivities(auth.simklAccessToken, modelContainer: context.container)
+      if episode?.episode != nil && episode?.season != nil {
+        HStack {
+          Text("S\(episode?.season ?? 0) E\(episode?.episode ?? 0)")
+            .font(.caption)
+            .bold()
+          Text(episode?.date?.formattedEpisodeDate() ?? "")
+            .font(.caption)
+          Spacer()
         }
-      }) {
-        Text(hasWatched ? "Watched" : "Watch")
-          .frame(maxWidth: .infinity)
-          .padding()
-          .bold()
-          .background(hasWatched ? Color.green.opacity(0.2) : Color.gray.opacity(0.2))
-          .foregroundColor(
-            hasWatched ? colorScheme == .dark ? Color.green : Color.green.darker() : colorScheme == .dark ? Color.gray : Color.gray.darker()
-          )
-          .cornerRadius(8)
+        .padding([.top, .bottom], 2)
+      }
+
+      if episode?.episode != nil && episode?.season != nil {
+        Button(action: {
+          Task {
+            if !hasWatched {
+              await AnimeDetailView.markEpisodeWatched(
+                auth.simklAccessToken,
+                animeDetails?.title ?? "",
+                simklId,
+                episode?.season ?? 0,
+                episode?.episode ?? 0
+              )
+              if let episode = episode {
+                if let index = animeWatchlist?.seasons?.firstIndex(where: { $0.number == episode.season }) {
+                  if let episodeIndex = animeWatchlist?.seasons?[index].episodes?.firstIndex(where: { $0.number == episode.episode }) {
+                    animeWatchlist?.seasons?[index].episodes?[episodeIndex].watched = true
+                  }
+                }
+              }
+            } else {
+              await AnimeDetailView.markEpisodeUnwatched(
+                auth.simklAccessToken,
+                animeDetails?.title ?? "",
+                simklId,
+                episode?.season ?? 0,
+                episode?.episode ?? 0
+              )
+              if let episode = episode {
+                if let index = animeWatchlist?.seasons?.firstIndex(where: { $0.number == episode.season }) {
+                  if let episodeIndex = animeWatchlist?.seasons?[index].episodes?.firstIndex(where: { $0.number == episode.episode }) {
+                    animeWatchlist?.seasons?[index].episodes?[episodeIndex].watched = false
+                  }
+                }
+              }
+            }
+            await syncLatestActivities(auth.simklAccessToken, modelContainer: context.container)
+          }
+        }) {
+          Text(hasWatched ? "Watched" : "Watch")
+            .frame(maxWidth: .infinity)
+            .padding()
+            .bold()
+            .background(hasWatched ? Color.green.opacity(0.2) : Color.gray.opacity(0.2))
+            .foregroundColor(
+              hasWatched ? colorScheme == .dark ? Color.green : Color.green.darker() : colorScheme == .dark ? Color.gray : Color.gray.darker()
+            )
+            .cornerRadius(8)
+        }
       }
 
       if let description = episode?.description {
